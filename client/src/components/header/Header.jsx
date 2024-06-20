03f30365a7a56f9ec98f95468cabe0f91e03d0b9
notPHPuser <artem2005dm@mail.ru>
2024-06-20 03:10:48 +0300

    nothing

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useRef, useEffect } from 'react';
+import React, { useState, useRef } from 'react';
 import PhoneInput from 'react-phone-input-2';
 import 'react-phone-input-2/lib/style.css';
 import './Header.css';
